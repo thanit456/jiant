8f75b9af69d0f411bea558d1650af03858ae4f2b
Thanit Tativannarat <too_w_o@hotmail.com>
2021-06-03 18:46:46 +0700

    add camembert tokenizer and model architecture for using wangchanberta

--- jiant/shared/model_resolution.py
+++ jiant/shared/model_resolution.py
@@ -15,6 +15,7 @@ class ModelArchitectures(Enum):
     MBART = "mbart"
     ELECTRA = "electra"
     DEBERTAV2 = "deberta-v2"
+    CAMEMBERT = "camembert"
 
     @classmethod
     def from_model_type(cls, model_type: str):
@@ -32,6 +33,7 @@ TOKENIZER_CLASS_DICT = BiDict(
         ModelArchitectures.MBART: transformers.MBartTokenizer,
         ModelArchitectures.ELECTRA: transformers.ElectraTokenizer,
         ModelArchitectures.DEBERTAV2: transformers.DebertaV2Tokenizer,
+        ModelArchitectures.CAMEMBERT: transformers.CamembertTokenizer
     }
 )
 
